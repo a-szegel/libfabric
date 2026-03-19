ea94793f1d68c1cd0e3bf59243b845f291bc4643
Seth Zegelstein <szegel@amazon.com>
2026-03-19 16:05:44 -0600

    Update UT Failure
    Signed-off-by: Seth Zegelstein <szegel@amazon.com>

--- prov/efa/src/rdm/efa_rdm_pke_cmd.c
+++ prov/efa/src/rdm/efa_rdm_pke_cmd.c
@@ -465,9 +465,7 @@ void efa_rdm_pke_handle_tx_error(struct efa_rdm_pke *pkt_entry, int prov_errno)
 			}
 			efa_rdm_pke_release_tx(pkt_entry);
 			efa_proto_tx_release(txe);
-		}
-
-		if (prov_errno == EFA_IO_COMP_STATUS_REMOTE_ERROR_RNR) {
+		} else if (prov_errno == EFA_IO_COMP_STATUS_REMOTE_ERROR_RNR) {
 			if (ep->handle_resource_management == FI_RM_DISABLED) {
 				/*
 				 * Write an error to the application for RNR when resource
